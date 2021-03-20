f8a9cfea70b0d1a246963be82470117533ed4487
Patrick Curran <>
2021-03-20 22:03:42 +0000

    Mongoose set up sign up and login in with error handling using boom

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const Hapi = require('@hapi/hapi');
-const Inert = require('@hapi/inert');
-const Vision = require('@hapi/vision');
-const Handlebars = require('handlebars');
+
+const Hapi = require("@hapi/hapi");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+const Handlebars = require("handlebars");
 const Cookie = require("@hapi/cookie");
-const env = require('dotenv');
 require('./app/models/db');
+const env = require('dotenv');
 
 
 
@@ -24,32 +25,28 @@ async function init() {
     await server.register(Cookie);
     server.views({
         engines: {
-            hbs: require('handlebars'),
+            hbs: require("handlebars"),
         },
         relativeTo: __dirname,
-        path: './app/views',
-        layoutPath: './app/views/layouts',
-        partialsPath: './app/views/partials',
+        path: "./app/views",
+        layoutPath: "./app/views/layouts",
+        partialsPath: "./app/views/partials",
         layout: true,
         isCached: false,
     });
-
-    server.auth.strategy('session', 'cookie', {
+    server.auth.strategy("session", "cookie", {
         cookie: {
             name: process.env.cookie_name,
             password: process.env.cookie_password,
-            isSecure: false,
+            isSecure: false
         },
-        redirectTo: '/',
+        redirectTo: "/",
     });
-
-    server.auth.default('session');
-
-    server.route(require('./routes'));
+    server.auth.default("session");
+    server.route(require("./routes"));
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
 }
-
 process.on('unhandledRejection', (err) => {
     console.log(err);
     process.exit(1);
