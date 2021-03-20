6ffa53b0c6c02a31613bfedc84f1ec41396bb0b6
pa5455 <patrickcurran48@hotmail.com>
2021-03-20 14:31:34 +0000

    first commit basic  set up

---
+++ routes.js
@@ -0,0 +1,15 @@
+const Pois = require('./app/controllers/pois');
+
+module.exports = [{ method: 'GET', path: '/', config: Pois.index },
+    {
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            directory: {
+                path: './public',
+            },
+        },
+    },
+    { method: "GET", path: "/signup", config: Pois.signup },
+    { method: "GET", path: "/login", config: Pois.login },
+];
