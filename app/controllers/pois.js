29a5614c53720caf058e2916f99d30f97d7d129c
Patrick Curran <>
2021-03-20 15:59:40 +0000

    Addition of handlebars and hapi vision Views updated with partials

--- app/controllers/pois.js
+++ app/controllers/pois.js
@@ -1,17 +1,17 @@
 const Pois = {
     index: {
         handler: function (request, h) {
-            return h.file('./app/views/main.html');
+            return h.view('main', { title: 'Welcome to Farm Sales POI' });
         },
     },
     signup: {
         handler: function (request, h) {
-            return h.file('./app/views/signup.html');
+            return h.view('signup', { title: 'Sign up for POI' });
         },
     },
     login: {
         handler: function (request, h) {
-            return h.file('./app/views/login.html');
+            return h.view('login', { title: 'Login to Donations' });
         },
     },
 };
--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const Hapi = require('@hapi/hapi');
 const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
+const Handlebars = require('handlebars');
 
 const server = Hapi.server({
     port: 3000,
@@ -10,6 +12,19 @@ const server = Hapi.server({
 
 async function init() {
     await server.register(Inert);
+    await server.register(Vision);
+    server.views({
+        engines: {
+            hbs: require('handlebars'),
+        },
+        relativeTo: __dirname,
+        path: './app/views',
+        layoutPath: './app/views/layouts',
+        partialsPath: './app/views/partials',
+        layout: true,
+        isCached: false,
+    });
+
     server.route(require('./routes'));
     await server.start();
     console.log(`Server running at: ${server.info.uri}`);
